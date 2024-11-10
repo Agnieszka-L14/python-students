f8f84f1b8b8043d3e6b331ab71bf859e236ab3e8
alichota <aga.lichota@gmail.com>
2024-11-10 15:00:49 +0100

    zmiana

--- names.py
+++ names.py
@@ -1,2 +1,10 @@
-print("Mam na imię...")  # tutaj wpisz swoje imię
+name ="Agnieszka"
+print("Mam na imię ", name)
 # i dodaj jeszcze jakiś ciekawy kod
+names=[]
+
+number = int(input("Podaj liczbe imion , które chcesz wprowadzić : " ))
+for i in range (number):
+    name = input("Podaj imię użytkownika : ")
+    names.append(name)
+print("Podano nastepujace imiona : ",names)
